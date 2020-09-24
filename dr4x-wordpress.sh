63ca6e9de135597db6e75d6201773224674e69e7
Charlotte Wengi <71440287+mandr4x@users.noreply.github.com>
2020-09-24 03:12:23 -0400

    Update dr4x-wordpress.sh

--- dr4x-wordpress.sh
+++ dr4x-wordpress.sh
@@ -6,5 +6,6 @@ clear
 echo -e "Wordlist: "; read host
 cat $host |parallel -j50 'request=$(timeout 5 curl -k -s {} |grep "WordPress" |head -n1 |cut -d ">" -f1 |cut -d "<" -f2 |cut -d "/" -f1 |cut -d "=" -f3 |cut -d " " -f2 |xargs -0)
 echo -e "{} - $request"' >> word
-sed 's/\"//g' word >> wordpress
-echo "" >> word ; rm -rf word
+cat word |grep '"' >> word2
+sed 's/\"//g' word2 >> wordpress
+echo "" > word ; echo "" > word2 ; rm -rf word word2
