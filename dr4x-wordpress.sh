2f97275318de12246a52f761fb83775a16611997
Charlotte Wengi <71440287+mandr4x@users.noreply.github.com>
2020-09-24 03:07:40 -0400

    Update dr4x-wordpress.sh

--- dr4x-wordpress.sh
+++ dr4x-wordpress.sh
@@ -2,16 +2,9 @@
 
 # Coded by Mandr4x #
 
+clear
 echo -e "Wordlist: "; read host
-for i in `cat $host`
-do
-request=$(timeout 5 curl -s http://$i/wp-admin/admin-ajax.php |grep -oP "0")
-request2=$(timeout 5 curl -k -s https://$i/wp-admin/admin-ajax.php |grep -oP "0")
-if [ "$request" = "0" ] || [ "$request2" = "0" ]
-then
-echo -e "\033[01;32m$i - Run WordPress\033[01;32m"
-echo -e "$i" >> wp
-else
-echo -e "\033[01;31m$i - Not Run WordPress\033[01;31m"
-fi
-done
+cat $host |parallel -j50 'request=$(timeout 5 curl -k -s {} |grep "WordPress" |head -n1 |cut -d ">" -f1 |cut -d "<" -f2 |cut -d "/" -f1 |cut -d "=" -f3 |cut -d " " -f2 |xargs -0)
+echo -e "{} - $request"' >> word
+sed 's/\"//g' word >> wordpress
+echo "" >> word ; rm -rf word
