8ceba3285149da1f369f12b8eb4e76b23adc4c6a
Charlotte Wengi <71440287+mandr4x@users.noreply.github.com>
2020-09-24 03:06:58 -0400

    Update dr4x-joomla.sh

--- dr4x-joomla.sh
+++ dr4x-joomla.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
-
+clear
 # Coded by Mandr4x #
 
 echo -e "wordlist: "; read host
-for i in `cat $host`
-do
-request=$(timeout 5 curl -s http://$i/administrator/manifests/files/joomla.xml |grep -oP "joomla.org" |head -n1)
-request2=$(timeout 5 curl -s -k https://$i/administrator/manifests/files/joomla.xml |grep -oP "joomla.org" |head -n1)
+
+cat $host |parallel -j100 'request=$(timeout 5 curl -s http://{}/administrator/manifests/files/joomla.xml |grep -oP "joomla.org" |head -n1)
+request2=$(timeout 5 curl -s -k https://{}/administrator/manifests/files/joomla.xml |grep -oP "joomla.org" |head -n1)
 if [ "$request" = "joomla.org" ] || [ "$request2" = "joomla.org" ]
 then
-echo -e "\033[01;32m$i - RUN JOOMLA\033[01;32m"
-echo -e "$i" >> jm
+echo -e "\033[01;32m{} - RUN JOOMLA\033[01;32m"
+echo -e "$i" >> joomla
 else
-echo -e "\033[01;31m$i - NOT RUN JOOMLA\033[01;31m"
-fi
-done
+echo -e "\033[01;31m{} - NOT RUN JOOMLA\033[01;31m"
+fi'
